04a2fee62cfeb7802b5f797109da7ddc02fb9d89
dextrux <titolucas1@live.com>
2024-04-21 18:03:53 -0300

    Fim da parte visual do menu
    Finalização das transições entre menus na unity.

--- Assets/Scripts/UI/SafeArea.cs
+++ Assets/Scripts/UI/SafeArea.cs
@@ -29,20 +29,11 @@ public class SafeArea : MonoBehaviour
         minAnchor = safeAreaRect.position;
         maxAnchor = minAnchor + safeAreaRect.size;
 
-
-        // Essas linhas normalizam as coordenadas da âncora mínima (posição) para o intervalo [0, 1].
-        // Dividimos as coordenadas pelo tamanho da tela(largura e altura) para obter valores proporcionais.
         minAnchor.x /= Screen.width;
         minAnchor.y /= Screen.height;
-        Debug.Log("MinAnchor: " + minAnchor);
         maxAnchor.x /= Screen.width;
         maxAnchor.y /= Screen.height;
-        Debug.Log("MaxAnchor: " + maxAnchor);
 
-
-        //Essas linhas definem as âncoras mínima e máxima do RectTransform.
-        //As âncoras controlam como o objeto se ajusta à tela quando a resolução muda.
-        //Ao definir essas âncoras com base na área segura da tela, o objeto se ajustará automaticamente para caber na área visível.
         rectTransform.anchorMin = minAnchor;
         rectTransform.anchorMax = maxAnchor;
     }
